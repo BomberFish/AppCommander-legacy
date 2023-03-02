3622c33dd86be1c78350a511fdfcf7bbd58eb2a3
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-02 18:14:11 -0500

    Initial Commit

---
+++ Cache/CacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheApp.swift
+//  Cache
+//
+//  Created by Hariz Shirazi on 2023-03-02.
+//
+
+import SwiftUI
+
+@main
+struct CacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cache/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cache
+//
+//  Created by Hariz Shirazi on 2023-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
