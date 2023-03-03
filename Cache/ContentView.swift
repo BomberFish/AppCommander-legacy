761fbabbb8396f4331b40a277eb3599945a5e97b
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-02 19:41:44 -0500

    mini refractor

--- Cache/ContentView.swift
+++ Cache/ContentView.swift
@@ -17,46 +17,6 @@ struct ContentView: View {
             .navigationTitle("Caché")
         }
     }
-    struct AppCell: View {
-        var imageName: String
-        var bundleid: String
-        var title: String
-        
-        var body: some View {
-            HStack(alignment: .center) {
-                Group {
-                        if imageName != "" {
-                            Image(imageName)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                        }
-                }
-                .cornerRadius(10)
-                .frame(width: 48, height: 48)
-                
-                VStack {
-                    HStack {
-                        Button(action: {
-                            print("lol")
-                        }) {
-                            Text(title)
-                                .fontWeight(.bold)
-                                .font(.headline)
-                        }
-                        .padding(.horizontal, 6)
-                        Spacer()
-                    }
-                    HStack {
-                        Text(bundleid)
-                            .padding(.horizontal, 6)
-                            .font(.footnote)
-                        Spacer()
-                    }
-                }
-            }
-            .foregroundColor(.accentColor)
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
