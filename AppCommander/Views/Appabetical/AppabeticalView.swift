8dcd94c7885ca9db01d3f24bc5fbc82f317ecec2
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-24 10:37:01 -0400

    some appabetical changes

--- AppCommander/Views/Appabetical/AppabeticalView.swift
+++ AppCommander/Views/Appabetical/AppabeticalView.swift
@@ -28,110 +28,55 @@ struct AppabeticalView: View {
                         MultiSelectPickerView(pages: IconStateManager.getPages(), selectedItems: $selectedItems, pageOp: $pageOp).navigationBarTitle("", displayMode: .inline)
                     }, label: {
                         HStack {
-                            Text("Select Pages")
+                            Label("Select Pages", systemImage: "apps.iphone")
                             Spacer()
                             Text(selectedItems.isEmpty ? "None" : selectedItems.map { String($0 + 1) }.joined(separator: ", ")).foregroundColor(.secondary)
                         }
                     })
-                    Picker("Ordering", selection: $sortOp) {
+                    Picker(selection: $sortOp, label: Label("Ordering", systemImage: "line.3.horizontal.decrease.circle")) {
                         Text("A-Z").tag(IconStateManager.SortOption.alphabetically)
                         Text("Z-A").tag(IconStateManager.SortOption.alphabeticallyReversed)
                         Text("Color").tag(IconStateManager.SortOption.color)
                     }.onChange(of: sortOp, perform: {nv in if nv == .color && folderOp == .alongside { folderOp = .separately }})
-                    Picker("Pages", selection: $pageOp) {
+                    Picker(selection: $pageOp, label: Label("Pages", systemImage: "rectangle.split.3x1")) {
                         Text("Sort pages independently").tag(IconStateManager.PageSortingOption.individually)
                         Text("Sort apps across pages").tag(IconStateManager.PageSortingOption.acrossPages)
                     }
-                    Picker("Folders", selection: $folderOp) {
+                    Picker(selection: $folderOp, label: Label("Folders", systemImage: "folder")) {
                         Text("Retain current order").tag(IconStateManager.FolderSortingOption.noSort)
                         if (sortOp == .alphabetically || sortOp == .alphabeticallyReversed) {
                             Text("Sort mixed with apps").tag(IconStateManager.FolderSortingOption.alongside)
                         }
                         Text("Sort separate from apps").tag(IconStateManager.FolderSortingOption.separately)
                     }
-                    Picker("Widgets", selection: $widgetOp) {
+                    Picker(selection: $widgetOp, label: Label("Widgets", systemImage: "rectangle")) {
                         Text("Move to top").tag(IconStateManager.WidgetOptions.top)
                     }
-                    Button("Sort Apps") {
-                        sortPage()
-                    }.disabled(selectedItems.isEmpty)
-                }
-                Section(footer: Text((fm.fileExists(atPath: savedLayoutUrl.path) ?  "The previously saved layout will be overwritten." : "It is recommended you save your current layout before experimenting as only one undo is possible." ) + "\n\nVersion \(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "pirate version bozo®")")) {
-                    Button("Undo Last Sort") {
-                        restoreBackup()
-                    }.disabled(!fm.fileExists(atPath: plistUrlBkp.path))
-                    Button("Restore Saved Layout") {
-                        restoreLayout()
-                    }.disabled(!fm.fileExists(atPath: savedLayoutUrl.path))
-                    Button("Back Up Current Layout") {
-                        saveLayout()
-                    }
-                }
-            }
-            .navigationTitle("Appabetical")
-            .toolbar {
-                // Credit to SourceLocation
-                // https://github.com/sourcelocation/AirTroller/blob/main/AirTroller/ContentView.swift
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
                     Button(action: {
-                        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                        openURL(URL(string: "https://discord.gg/VyVcNjRMeg")!)
-                    }) {
-                        Image("discord")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 24, height: 24)
-                    }
-                    Menu {
-                        Button {
-                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                            openURL(URL(string: "https://github.com/Avangelista/Appabetical")!)
-                        } label: {
-                            Label("Source Code", systemImage: "shippingbox")
-                        }
-                        Button {
-                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                            openURL(URL(string: "https://github.com/Avangelista")!)
-                        } label: {
-                            Label("Avangelista", systemImage: "person")
-                        }
-                        Button {
-                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                            openURL(URL(string: "https://github.com/sourcelocation")!)
-                        } label: {
-                            Label("sourcelocation", systemImage: "person")
-                        }
-                    } label: {
-                        Image("github")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                    }
-                    Menu {
-                        Button {
-                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                            openURL(URL(string: "https://ko-fi.com/avangelista")!)
-                        } label: {
-                            Label("Avangelista", systemImage: "1.circle")
-                        }
-                        
-                        Button {
-                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                            openURL(URL(string: "https://ko-fi.com/sourcelocation")!)
-                        } label: {
-                            Label("sourcelocation", systemImage: "2.circle")
-                        }
-                    } label: {
-                        Image(systemName: "heart.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                    }
+                        sortPage()
+                    }, label: {
+                        Label("Sort Apps", systemImage: "checkmark.circle")
+                    }).disabled(selectedItems.isEmpty)
+                }
+                Section(footer: Text((fm.fileExists(atPath: savedLayoutUrl.path) ?  "The previously saved layout will be overwritten." : "It is recommended you save your current layout before experimenting as only one undo is possible." ))) {
+                    Button(action: {
+                        restoreBackup()
+                    }, label: {
+                        Label("Undo Last Sort", systemImage: "arrow.uturn.backward")
+                    }).disabled(!fm.fileExists(atPath: plistUrlBkp.path))
+                    Button(action: {
+                        restoreLayout()
+                    }, label: {
+                        Label("Restore Saved Layout", systemImage: "square.and.arrow.up.on.square")
+                    }).disabled(!fm.fileExists(atPath: savedLayoutUrl.path))
+                    Button(action: {
+                        saveLayout()
+                    }, label: {
+                        Label("Back Up Current Layout", systemImage: "square.and.arrow.down.on.square")
+                    })
                 }
             }
-            .onAppear {
-                
-            }
+            .navigationTitle("Home Screen")
         }
     }
     
