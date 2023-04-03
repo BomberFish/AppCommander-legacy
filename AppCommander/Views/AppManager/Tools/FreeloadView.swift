27017785e6c4287c51d74a8b1a3a76477e47f891
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-04-02 21:46:56 -0400

    whoops

--- AppCommander/Views/AppManager/Tools/FreeloadView.swift
+++ AppCommander/Views/AppManager/Tools/FreeloadView.swift
@@ -11,32 +11,31 @@ struct FreeloadView: View {
     @State var inProgress = false
     @State var success = false
     var body: some View {
-            List {
-                Section {
-                    Button(action: {
-                        inProgress = true
-                        Haptic.shared.play(.medium)
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                            success = patch_installd()
-                            
-                            if success {
-                                UIApplication.shared.alert(title: "Success", body: "Successfully patched installd!", withButton: true)
-                                Haptic.shared.notify(.success)
-                                inProgress = false
-                            } else {
-                                UIApplication.shared.alert(title: "Failure", body: "Failed to patch installd!", withButton: true)
-                                Haptic.shared.notify(.error)
-                                inProgress = false
-                            }
+        List {
+            Section {
+                Button(action: {
+                    inProgress = true
+                    Haptic.shared.play(.medium)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        success = patch_installd()
+
+                        if success {
+                            UIApplication.shared.alert(title: "Success", body: "Successfully patched installd!", withButton: true)
+                            Haptic.shared.notify(.success)
+                            inProgress = false
+                        } else {
+                            UIApplication.shared.alert(title: "Failure", body: "Failed to patch installd!", withButton: true)
+                            Haptic.shared.notify(.error)
+                            inProgress = false
                         }
-                    }, label: {
-                        Label("Apply", systemImage: "checkmark.seal")
                     }
-                    )
-                }
-                    .navigationTitle("Remove three-app limit")
+                }, label: {
+                    Label("Apply", systemImage: "checkmark.seal")
+                })
             }
+        }
         .disabled(inProgress)
+        .navigationTitle("Remove three-app limit")
     }
 }
 
