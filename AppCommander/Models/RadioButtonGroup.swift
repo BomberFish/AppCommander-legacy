54aff4973c3c9efebd6ee86b2cc8070b4a7622c0
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-06-30 15:23:57 -0400

    Cozy & Compact views

---
+++ AppCommander/Models/RadioButtonGroup.swift
@@ -0,0 +1,18 @@
+//
+//  RadioButtonGroup.swift
+//  AppCommander
+//
+//  Created by Hariz Shirazi on 2023-06-29.
+//
+
+import SwiftUI
+
+struct RadioButtonGroup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RadioButtonGroup()
+}
---
+++ AppCommander/Views/Settings/ViewSettings.swift
@@ -0,0 +1,18 @@
+//
+//  ViewSettings.swift
+//  AppCommander
+//
+//  Created by Hariz Shirazi on 2023-06-29.
+//
+
+import SwiftUI
+
+struct ViewSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewSettings()
+}
